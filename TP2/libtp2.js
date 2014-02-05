89251336ce666aa36c0a2394c66c881b92854a37
Anis Benyoub <Anis@MacBook-Pro-de-Anis.local>
2014-02-05 00:27:53 -0500

    Avancement tp

--- TP2/libtp2.js
+++ TP2/libtp2.js
@@ -1,5 +1,46 @@
-// Certaines de ces fonctions ont été récupérées du TP1 
-// elle permettent de gérer les differentes opérations openGL
+
+
+function computeNormal(p1, p2, p3)
+{
+    var v1 = vec3.create();
+    vec3.subtract(p1, p2, v1);
+    var v2 =vec3.create();
+    vec3.subtract(p1, p3,v2);
+    var norm = vec3.create();
+    vec3.cross(v1, v2,norm);
+    vec3.normalize(norm);
+    return norm;
+}
+function handleKeyDown(event) 
+{
+    currentlyPressedKeys[event.keyCode] = true;
+    if (String.fromCharCode(event.keyCode) == "Z") 
+    {
+        camera.viewMatrix=mat4.translate(camera.viewMatrix, [0,0,5]);
+    }
+    else if (String.fromCharCode(event.keyCode) == "S") 
+    {
+        camera.viewMatrix=mat4.translate(camera.viewMatrix, [0,0,-5]);
+    }
+    else if (String.fromCharCode(event.keyCode) == "Q") 
+    {
+        camera.viewMatrix=mat4.translate(camera.viewMatrix, [5,0,0]);
+    }
+    else if (String.fromCharCode(event.keyCode) == "D") 
+    {
+        camera.viewMatrix=mat4.translate(camera.viewMatrix, [-5,0,0]);
+    }
+    camera.isOutDated =  true;
+}
+
+function handleKeyUp(event) 
+{
+    currentlyPressedKeys[event.keyCode] = false;
+}
+
+function handleMouseMove(event) 
+{
+}
 
 // Initialisation du contexte GL
 function initGL(parCanvas) 
@@ -10,6 +51,9 @@ function initGL(parCanvas)
         gl = parCanvas.getContext("webgl");
         gl.viewportWidth = parCanvas.width;
         gl.viewportHeight = parCanvas.height;
+        document.onkeydown = handleKeyDown;
+        document.onkeyup = handleKeyUp;
+        document.onmousemove = handleMouseMove;
     } 
     catch (e) 
     {
@@ -21,7 +65,8 @@ function initGL(parCanvas)
     }
 }
 
-// Verification du shader
+// Fonction shader
+// Vérification shader
 function checkShader(parShader)
 {
     if (!gl.getShaderParameter(parShader, gl.COMPILE_STATUS)) 
@@ -68,125 +113,41 @@ function compileShader(gl, id, parShaderType)
     checkShader(shader);
     return shader;
 }   
-function generateProgram(parVertexFile, parVertexFile)
+
+
+// Inits
+function generateProgram(parVertex, parFragment) 
 {
     // Compilation fragment shader
     var fragmentShader = compileShader(gl, parVertex, gl.VERTEX_SHADER);
     // Compilation du vertex shader
     var vertexShader = compileShader(gl, parFragment, gl.FRAGMENT_SHADER);
     // On crée le programme
-    shaderProgram = gl.createProgram();
+    var shaderProg = gl.createProgram();
     // On attache le vertex shader
-    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProg, vertexShader);
     // On attache le fragment shader
-    gl.attachShader(shaderProgram, fragmentShader);
+    gl.attachShader(shaderProg, fragmentShader);
     // On link le programme
-    gl.linkProgram(shaderProgram);
+    gl.linkProgram(shaderProg);
     // On vérifie le programme
-    checkProgram(shaderProgram);
+    checkProgram(shaderProg);
     //On utilise le programme
-    gl.useProgram(shaderProgram);
-
-    return shaderProgram;
+    gl.useProgram(shaderProg);
+    // retourn les données
+    return shaderProg;
 }
 
-function initUsualUniforms(parProgam)
+function bindProgram(parShader)
 {
-    // On bind le vertex buffer object de position
-    parProgam.vertexPositionAttribute = gl.getAttribLocation(parProgam, "position");
-    gl.enableVertexAttribArray(parProgam.vertexPositionAttribute);
-
-    // On bind le vertex buffer object de normale
-    parProgam.vertexNormalAttribute = gl.getAttribLocation(parProgam, "normal");
-    gl.enableVertexAttribArray(parProgam.vertexNormalAttribute);
-
-    // On bind le vertex buffer object de tex coord
-    parProgam.vertexTexCoordAttribute = gl.getAttribLocation(parProgam, "texcoord");
-    gl.enableVertexAttribArray(parProgam.vertexTexCoordAttribute);                
-
-    // On récupère l'index la matrice de projection
-    parProgam.projMatrix = gl.getUniformLocation(parProgam, "proj");
-    // On récupère l'index la matrice de vue
-    parProgam.viewMatrix = gl.getUniformLocation(parProgam, "view");
-    // On récupère l'index de la normal matrix
-    parProgam.normalMatrix = gl.getUniformLocation(parProgam, "normalM");
-
-    // On récupère l'index la matrice de modele
-    parProgam.modelMatrix = gl.getUniformLocation(parProgam, "model");
+    gl.useProgram(parShader);
 }
 
-function injectModelMatrix(parProgam, parModel)
+function unbindProgram()
 {
-    gl.uniformMatrix4fv(parProgam.modelMatrix , false, parModel);
+    gl.useProgram(null);
 }
 
-function injectProjectionMatrix(parProgam, parProjection)
-{
-    gl.uniformMatrix4fv(parProgam.projMatrix , false, parProjection);
-}
-
-function injectView(parProgam, parView)
-{
-    gl.uniformMatrix4fv(parProgam.viewMatrix , false, parView);
-}
-
-function injectNormalMatrix(parProgam, parNormalMatrix)
-{
-    gl.uniformMatrix4fv(parProgam.normalMatrix , false, parNormalMatrix);
-}
-
-function injectMatrix(parProgam, parMatrixName, parMatrix)
-{
-	// Recuperation de l'id
-	var matrixID =  gl.getUniformLocation(parProgam, parMatrixName);
-    // Injection de la matrix
-    gl.uniformMatrix4fv(matrixID, false, parMatrix);
-}
-
-function injectVec3(parProgam, parVec3Name, parVec3)
-{
-	// Recuperation de l'id
-	var vecID =  gl.getUniformLocation(parProgam, parVec3Name);
-    // Injection du vec3
-    gl.uniform3fv(vecID, false, parVec3);
-}
-
-function injectVec3(parProgam, parVec4Name, parVec4)
-{
-	// Recuperation de l'id
-	var vecID =  gl.getUniformLocation(parProgam, parVec4Name);
-    // Injection du vec4
-    gl.uniform3fv(vecID, false, parVec4);
-}
-
-function createBuffersVI(parObj, parVertexList, parIndexList)
-{
-   // Création du buffer de position
-    var vertexPositionBuffer = gl.createBuffer();
-    // On bind le buffer de position
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    //On copie les données sur ke GPU
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(parVertexList), gl.STATIC_DRAW);
-    // 3 données par position
-    vertexPositionBuffer.itemSize = 3;
-    // Nombre de points
-    vertexPositionBuffer.numItems = parVertexList.length / 3;
-    // Copie dans la structure sphère
-    parObj.vertexPositionBuffer = vertexPositionBuffer;
-
-    // Création de l'IBO
-    var vertexIndexBuffer = gl.createBuffer();
-    // ON bind ke buffer
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
-    // ON copie les données sur le GPU
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STREAM_DRAW);
-    // 3 Données par triangle
-    vertexIndexBuffer.itemSize = 3;
-    // Nombre d'indexes
-    vertexIndexBuffer.numItems = indexData.length;
-    // On copie dans la structure sphère
-    parObj.vertexIndexBuffer = vertexIndexBuffer;
-}
 
 function createBuffersVINT(parObj, parVertexList, parIndexList, parNormalList, parTexCoordList)
 {
@@ -241,3 +202,99 @@ function createBuffersVINT(parObj, parVertexList, parIndexList, parNormalList, p
     // On le copie dans la strcture de l'objet
     parObj.vertexTextureCoordBuffer = vertexTextureCoordBuffer;
 }
+
+function injectModelMatrix(parProgam, parModel)
+{
+    gl.uniformMatrix4fv(parProgam.modelMatrix , false, parModel);
+}
+
+function injectProjectionMatrix(parProgam, parProjection)
+{
+    gl.uniformMatrix4fv(parProgam.projMatrix , false, parProjection);
+}
+
+function injectView(parProgam, parView)
+{
+    gl.uniformMatrix4fv(parProgam.viewMatrix , false, parView);
+}
+
+function injectNormalMatrix(parProgam, parNormalMatrix)
+{
+    gl.uniformMatrix4fv(parProgam.normalMatrix , false, parNormalMatrix);
+}
+
+function injectMatrix(parProgam, parMatrixName, parMatrix)
+{
+    // Recuperation de l'id
+    var matrixID =  gl.getUniformLocation(parProgam, parMatrixName);
+    // Injection de la matrix
+    gl.uniformMatrix4fv(matrixID, false, parMatrix);
+}
+
+function injectVec3(parProgam, parVec3Name, parVec3)
+{
+    // Recuperation de l'id
+    var vecID =  gl.getUniformLocation(parProgam, parVec3Name);
+    // Injection du vec3
+    gl.uniform3fv(vecID, false, parVec3);
+}
+
+function injectVec3(parProgam, parVec4Name, parVec4)
+{
+    // Recuperation de l'id
+    var vecID =  gl.getUniformLocation(parProgam, parVec4Name);
+    // Injection du vec4
+    gl.uniform3fv(vecID, false, parVec4);
+}
+
+function CreateTexture() 
+{
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+ 
+    // Set up texture so we can render any size image and so we are
+    // working with pixels.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+ 
+    return texture;
+  }
+
+function CreateFrameBufferObject(parWidth, parHeight)
+{
+   
+    var colorTex = CreateTexture();
+    gl.bindTexture(gl.TEXTURE_2D, colorTex);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, parWidth, parHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+    var depthTex = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, depthTex);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, parWidth, parHeight);
+
+    // var depthTex = CreateTexture();
+    // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, parWidth, parHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+    var fbo = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    fbo.width = parWidth;
+    fbo.height = parHeight;
+
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, colorTex, 0);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthTex, 0);
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    return fbo;
+}
+
+function bindFBO(fbo)
+{
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+}
+
+function unbindFBO()
+{
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+}
+
